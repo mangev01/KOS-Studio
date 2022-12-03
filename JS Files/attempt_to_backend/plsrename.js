8ec94b9484c07baff2c3a4d7aa953e942b3b8ff0
Tyler Cerezo <tylercerezo@yahoo.com>
2022-12-02 22:21:13 -0500

    Add mongodb (#17)
    * Revert "we learned that we can tango with the... mongo"
    
    This reverts commit b45c3211eb4a40e482be8ef746cf26631b5ce00f.
    
    * added to mango to tango
    
    * i forgot to do the package thingy

--- JS Files/attempt_to_backend/plsrename.js
+++ JS Files/attempt_to_backend/plsrename.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose=require("mongoose")
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://127.0.0.1/test', {useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
